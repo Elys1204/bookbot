e30daed6a83dcae92a21d2cc9174eb98a225be58
Elys1204 <elysrazor@gmail.com>
2024-06-19 11:36:58 +0100

    Added clean reporting

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     
-    print(count_words(file_contents))
-    print(count_characters(file_contents))
+    word_count = count_words(file_contents)
+
+    sorted_list = count_characters(file_contents)
+    sorted_list.sort(reverse=True, key=sort_on)
     
+    print_report(word_count, sorted_list)
 
-
-
+    
+    
 def count_words(book):
     word_list = book.split()
     counter = 0
@@ -19,17 +22,37 @@ def count_words(book):
     return counter
 
 def count_characters(book):
-    character_dictionary = {}
+    char_dict = {}
     book_to_lower = book.lower()
     character_list = list(book_to_lower)
 
     for c in character_list:
-        if c in character_dictionary:
-            character_dictionary[c] += 1
-        else:
-            character_dictionary[c] = 1
+        if c.isalpha():
+            if c in char_dict:
+                char_dict[c] += 1
+            else:
+                char_dict[c] = 1
 
-    return character_dictionary
+    return dict_to_list(char_dict)
+
+def dict_to_list(char_dict):
+
+    char_dict_list=[]
+
+    for c, count in char_dict.items():
+        temp_dict = {'char': c, 'num': count}
+        char_dict_list.append(temp_dict)
+
+    return char_dict_list
+
+def sort_on(dict):
+    return dict["num"]
+
+def print_report(word_count, sorted_list):
+    print(f"----Begin report of Frankenstein----\n"
+        f"      Total Word Count = {word_count}")
+    for i in sorted_list:
+        print(f"The character {i['char']} appeared {i['num']} times")
 
 main()
 
